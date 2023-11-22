162447fdefcf4918654c1bf82b9102c8e209898b
chilak.kumar <chilak.kumar@gmail.com>
2023-11-22 07:56:49 +0000

    updated code for user input

--- main.py
+++ main.py
@@ -18,12 +18,29 @@ def justify_paragraph(paragraph, page_width):
 
     return justified_lines
 
-# Example Usage
-paragraph_input = "This is a sample paragraph that we want to justify based on a given page width."
-page_width_input = 20
+def get_user_input():
+    paragraph = input("Enter the paragraph: ")
+    while not paragraph:
+        print("Paragraph cannot be empty. Please enter a valid paragraph.")
+        paragraph = input("Enter the paragraph: ")
 
-justified_lines_output = justify_paragraph(paragraph_input, page_width_input)
+    page_width = input("Enter the page width: ")
+    while not page_width.isdigit() or int(page_width) <= 0:
+        print("Page width must be a positive integer. Please enter a valid page width.")
+        page_width = input("Enter the page width: ")
 
-# Print the justified lines with array index
-for i, line in enumerate(justified_lines_output):
-    print(f"Array [{i}] = {line}")
+    return paragraph, int(page_width)
+
+def main():
+    # Get user input
+    paragraph_input, page_width_input = get_user_input()
+
+    # Justify the paragraph
+    justified_lines_output = justify_paragraph(paragraph_input, page_width_input)
+
+    # Print the justified lines with array index
+    for i, line in enumerate(justified_lines_output):
+        print(f"[{i}] {line}")
+
+if __name__ == "__main__":
+    main()
